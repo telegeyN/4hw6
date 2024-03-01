9f5128db7896ba75e51d7e326025417b0ed47e59
telegeyN <ntelegey@gmail.com>
2024-03-01 15:00:12 +0600

    Добавила horizontal collection view. На всех устройствах дизайн выглядит хорошо, ничего не перескакивает и тд

---
+++ 4hw6/CollectionViewCell/HorizontalCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HorizontalCollectionViewCell.swift
+//  4hw6
+//
+//  Created by Telegey Nurbekova on 01/03/24.
+//
+
+import UIKit
+
+class HorizontalCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ 4hw6/Model/ModelForHorizontal.swift
@@ -0,0 +1,8 @@
+//
+//  ModelForHorizontal.swift
+//  4hw6
+//
+//  Created by Telegey Nurbekova on 01/03/24.
+//
+
+import Foundation
